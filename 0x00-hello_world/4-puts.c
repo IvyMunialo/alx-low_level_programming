e903c21179e3c5fa65ef19d209b4d36aff930498
IvyMunialo <ivy0095munialo@gmail.com>
2022-09-08 14:28:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 /**
  *main- is the entry point
  *Decription:Print to the s\o
- *Return (0)
+ *Return:(0)
  */
 
 int main(void)
 
 {
-  puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
