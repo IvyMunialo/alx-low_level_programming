a73598a176557ac77b2a37ddb17e3bd1f68863c8
IvyMunialo <ivy0095munialo@gmail.com>
2022-09-08 16:18:04 -0700

    no printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *main- prints followed by a new line, to the standard error
- *Description:print to standard error
- *Return:(1)
-*/
+ *main - Entry function
+ *Description : Print using the write funnction to display to stdio
+ *Return:1 if successful
+ */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 "));
-return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",	
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));  
+return (1);  
 }
