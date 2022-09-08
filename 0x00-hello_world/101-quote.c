d9ea8f51cdce97e966b2152f4d37da62031e596a
IvyMunialo <ivy0095munialo@gmail.com>
2022-09-08 16:06:13 -0700

    no printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main-prints followed by a new line, to the standard error
+ *Description:print to standard error
+ *Return:(1)
+*/
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 "));
+return (1);
+}
