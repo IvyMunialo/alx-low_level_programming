3aa472757a49ff258c2da209ec1ee3c94011d861
IvyMunialo <ivy0095munialo@gmail.com>
2022-09-14 12:53:39 -0700

    prints every minute of the day from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *jack_bauer - function that prints every minute of the day
+ *n starts from 00:00 to 23:59, hour loop counts hours,
+ *minutes loop counts minutes and resets minutes
+ *Return:always 0 (success)
+*/
+void jack_bauer(void)
+{
+int hours = 0;
+int minutes = 0;
+int hours_remainder;
+int minutes_remainder;
+while (hours <= 23)
+{
+while (minutes <= 59)
+{
+minutes_remainder = minutes % 10;
+hours_remainder = hours % 10;
+_putchar(hours / 10 + '0');
+_putchar(hours_remainder + '0');
+_putchar(":");
+_putcharminutes / 10 + '0');
+_putcharMinutes_remainder + '0');
+minutes++;
+_putchar('\n');
+}
+hours++;
+minutes = 0;
+}
+}
