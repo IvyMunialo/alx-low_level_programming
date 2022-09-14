26b22f4886f605d5a585e9310b4b818b1e96e368
IvyMunialo <ivy0095munialo@gmail.com>
2022-09-14 00:55:19 -0700

    compares and prints

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,21 +9,21 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar("+")
+putchar('+')
 }
 {
 return (1);
 }
 if (n == 0)
 {
-putchar("=")
+putchar('=')
 }
 {
 return (0);
 }
 else if (n < 0)
 {
-putchar("-")
+putchar('-')
 }
 {
 return (-1);
