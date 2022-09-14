106540ae18f8417e3b837b873545d3836eed2770
IvyMunialo <ivy0095munialo@gmail.com>
2022-09-14 00:50:35 -0700

    compares and prints

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_sign - compares and prints results
+ *@n: is the integer that will be used for the argument
+ *Return:always 0 (success)
+ */
+int print_sign(int n)
+{
+int n;
+if (n > 0)
+{
+putchar("+")
+}
+{
+return (1);
+}
+if (n == 0)
+{
+putchar("=")
+}
+{
+return (0);
+}
+else if (n < 0)
+{
+putchar("-")
+}
+{
+return (-1);
+}
+}
