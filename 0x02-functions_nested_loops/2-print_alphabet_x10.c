d8ac749dda6b6379b16b89bfb5ec3e828364270b
IvyMunialo <ivy0095munialo@gmail.com>
2022-09-13 23:51:23 -0700

    10 times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints the alphabet 10 times
+ *Return:always 0 (success)
+*/
+void print_alphabet_x10(void)
+{
+char c, i;
+for (i = 0; i <= 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+}
+}
