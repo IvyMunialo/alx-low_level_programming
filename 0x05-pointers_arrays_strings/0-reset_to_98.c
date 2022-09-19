cfaee51646bed75a8eb804ee0a6bef472e558e22
IvyMunialo <ivy0095munialo@gmail.com>
2022-09-19 05:15:56 -0700

    takes a pointer updates the value it points to

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -8,8 +8,8 @@ int main(void)
 {
 int n;
 n = 402;
-_putchar("value of 'n' : %d\n", n);
+printf("value of 'n' : %d\n", n);
 reset_to_98(&n);
-_putchar("value of 'n' : %d\n", n);
+printf("value of 'n' : %d\n", n);
 return (0);
 }
