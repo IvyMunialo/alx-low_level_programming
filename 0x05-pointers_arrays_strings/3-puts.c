2395fabfb5a4b5205b9235c3d63eb3e10fdb7e89
IvyMunialo <ivy0095munialo@gmail.com>
2022-09-19 12:22:52 -0700

    prints a string, followed by a new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 /**
  *_puts - prints a string, followed by a new line
- *@str: is the string toprint
- *successful: return no error
+ *@str: is the string to print
+ *
+ *Description: prints a string
+ *on success: return no error
 */
 void _puts(char *str)
 {
 int c = 0;
-{
 while (*(str + c) != '\0')
+{
 putchar (*(str + c));
 c++;
 }
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ *_puts - prints a string, followed by a new line
+ *@str: is the string toprint
+ *successful: return no error
+*/
