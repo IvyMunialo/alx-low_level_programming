a70d7e36e51c385d14350a387238dc22cb6cb926
IvyMunialo <ivy0095munialo@gmail.com>
2022-09-20 11:53:06 -0700

    prints the numbers, from 0 to 9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,13 +10,9 @@ void print_most_numbers(void)
 int a;
 for (a = '0'; a <= '9'; a++)
 {
-if (a == '2' && a == '4')
+if (a != 2 && a != 4)
 {
-continue;
-}
-else
-{
-putchar(a);
+putchar((a % 10) + '0');
 }
 }
 putchar('\n');
