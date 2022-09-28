e67884888c0f1fab6417e78bcdf7e5d868acc191
IvyMunialo <ivy0095munialo@gmail.com>
2022-09-28 12:21:22 -0700

    function that prints a string

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,6 +4,7 @@
  *Description:add all prototypes
  *Return:always 0 (success)
 */
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
