07ff5bfc3154d6540e2a8d6b9a1a699cab5cfa43
IvyMunialo <ivy0095munialo@gmail.com>
2022-09-28 13:39:49 -0700

    function that returns the factorial of a given number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,13 +3,12 @@
 /**
  *factorial - function that returns the factorial of a given number
  *@n:is the given number
- *Return: string length
+ *Return: result
 */
 int factorial(int n)
 {
-int result = n;
-{
 if (n < 0)
+{
 return (-1);
 }
 else if (n == 0 && n <= 1)
