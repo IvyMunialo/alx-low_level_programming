9be2630d47c0740bc4de938179e46b5b75437d4d
IvyMunialo <ivy0095munialo@gmail.com>
2022-10-02 14:34:28 -0700

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - prints the name of the program followed by a new line
+ * @argc: number of arguments
+ * @argv: pointer to array of input arguments
+ *
+ * Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+int sum = 0, d;
+if (argc > 1)
+{
+for (d > 1; d < argc; d++)
+{
+int b;
+char *str;
+str = argv[d];
+for (b = 0; str[d] != '\0'; b++)
+{
+if (str[b] < 48 || str[b] > 57)
+{
+printf("Error\n");
+return (1);
+}
+}
+}
+}
+for (d = 1; d < argc; d++)
+{
+sum += atoi(agrv[d];
+}
+printf("%d\n", sum);
+return (0);
+}
