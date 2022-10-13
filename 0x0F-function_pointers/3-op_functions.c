b4d8ebb0de695967d7e94ebb3a4d47c7365463f7
IvyMunialo <ivy0095munialo@gmail.com>
2022-10-13 15:23:27 -0700

    task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,17 +1,65 @@
 #include "3-calc.h"
-#include <stdio.h>
-#define ops 4
-
-int sum(int a, int b) {return (a+b);}
-int sub(int a, int b) {return (a-b);}
-int mult(int a, int b) {return (a*b);}
-int divi(int a, int b) {return (a/b);}
-int mod(int a, int b) { return (a%b);}
-int main()
+/**
+ * op_add - returns sum of 2 numbers
+ * @a: integer
+ * @b: integer
+ * retturn: sum
+ */
+int op_add(int a, int b)
 {
-int (*ptr2func[ops])(int, int) = {sum, sub, mult, divi, mod};
-int choice;
-int a, b;
-printf("%d", ptr2func[choice] (a, b));
-return 0;
+return a + b;
+}
+
+/**
+ * op_sub - subtracts 2 numbers
+ * @a: integer
+ * @b: integer
+ * returns: difference
+ */
+int op_sub(int a, int b)
+{
+return a - b;
+}
+
+/**
+ * ops_mult - multiplies 2 numbers
+ * @a: integer
+ * @b: integer
+ * return: prodeuct
+ */
+int op_mult(int a, int b)
+{
+return a * b;
+}
+
+/**
+ * ops_divi - divides 2 numbers
+ * @a; integer
+ * @b; integer
+ * returns; quotient
+ */
+int op_divi(int a, int b)
+{
+if (b == 0)
+{
+return ("Error\n");
+exit (100);
+}
+return a / b;
+}
+
+/**
+ * ops_mod - returns the remainder of division
+ * @a: integer
+ * @b: integer
+ * return: remainder
+ */
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit (100);
+}
+return a % B;
 }
