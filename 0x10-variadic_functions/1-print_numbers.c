d42f7f9e5a8652faa33373ab9dffe7cb9c2ec068
IvyMunialo <111011664+IvyMunialo@users.noreply.github.com>
2022-10-14 23:03:15 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ va_start(ap, n);
 for (count = 0; count < n; count++)
 {
 printf("%d", va_arg(ap, int));
-if (count != (n - 1) && separator != (NULL)
+if (count != (n - 1) && separator != NULL)
 printf("%s", separator);
 }
 printf("\n");
