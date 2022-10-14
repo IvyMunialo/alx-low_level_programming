20a59693ba261032542ffa201064a0473fdea324
IvyMunialo <111011664+IvyMunialo@users.noreply.github.com>
2022-10-14 22:44:27 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,11 +13,9 @@ va_list ap;
 unsigned int index;
 va_start(ap, n);
 for (index = 0; index < n; index++)
-{
 printf("%d", va_arg(ap, int);
 if (count != (n - 1) && separator != (NULL)
 printf("%s", separator);
-}
 printf("\n");
 va_end(ap);
 }
